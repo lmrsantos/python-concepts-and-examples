f5a9ae3137779082e04ef6342e76b049d11e11e0
Luiz Marcelo <34256230+lmrsantos@users.noreply.github.com>
2023-09-20 13:10:25 -0700

    add function as argument

--- nestedfunc.py
+++ nestedfunc.py
@@ -1,5 +1,6 @@
 # understanding nested function before talking about decorator (after understanding it check decorator.py)
 
+#NESTED Function
 def external(x):
     def internal(y):
         return x + y
@@ -7,5 +8,15 @@ def external(x):
 
 add_4 = external(4)
 result = add_4(6)
-print (result)
-print (type(add_4))
+print (f'Nested Function: {result}')
+
+# Function as ARGUMENT
+def sum_numbers(x, y):
+    return x+y
+
+def call_func(func, x, y):
+    return func(x, y)
+
+result = call_func(sum_numbers,4,6)
+
+print (f'Function as argument: {result}')
